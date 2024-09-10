13d36b62c925f48cb7bdc1a8b92617684ec43860
Aware man <110034401+JayeshThakare98@users.noreply.github.com>
2024-09-10 16:32:01 +0530

    com 1

--- SpringBoot2/UserService/src/main/java/com/user/UserServiceApplication.java
+++ SpringBoot2/UserService/src/main/java/com/user/UserServiceApplication.java
@@ -1,11 +1,13 @@
 package com.user;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
